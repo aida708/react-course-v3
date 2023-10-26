59357867f948e4edc3cdbc4a96f29d37a2caed01
Aida <aidasufaj00@gmail.com>
2023-10-26 19:03:00 +0200

    fetch data 03

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,31 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        //get the response
+        const response = await fetch(url);
+        const user = await response.json();
+        console.log(user);
+        setUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <>
+      const {(name, avatar_url, id)} = user;
+      <img src={avatar_url} />
+      <h1>{name}</h1>
+    </>
+  );
 };
 export default MultipleReturnsFetchData;
